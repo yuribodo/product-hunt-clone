3a51ff4a7e6463abda8c1dd998a4483be5d98ae0
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-05-24 17:20:44 -0400

    navbar initial design

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='h-[10vh] bg-gray-600'>
+      <div className='flex justify-between items-center h-full px-4'>
+        <div className='flex space-x-4'>
+          <input type="text" className='p-1' placeholder="Buscar..." />
+          <button type="button">Produtos</button>
+          <button type="button">Categorias</button>
+          <button type="button">Sobre</button>
+        </div>
+        <div className='flex space-x-2'>
+          <button type="button">Login</button>
+          <button type="button">Registro</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
