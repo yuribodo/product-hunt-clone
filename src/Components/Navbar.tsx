f4e8c19f6b86312ae4c2e324346cab44c575e898
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-05-26 14:57:23 -0400

    navbar animation

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SignedOut, SignInButton, SignUpButton } from "@clerk/clerk-react";
+import { motion } from 'framer-motion';
 
 interface NavbarProps {
   onSearchChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -8,27 +9,73 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ onSearchChange }) => {
   return (
-    <div className='h-[10vh] bg-gray-600'>
-      <div className='flex justify-between items-center h-full px-4'>
-        <div className='flex space-x-4 text-white'>
+    <motion.div 
+      className='h-[10vh] bg-gray-800 shadow-lg'
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className='flex justify-between items-center h-full px-6'>
+        <motion.div 
+          className='flex space-x-6 text-white'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.3 }}
+        >
           <input 
             type="text" 
-            className='p-1 rounded-lg text-black' 
+            className='p-2 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-blue-500' 
             placeholder="Buscar..." 
             onChange={onSearchChange}
           />
-          <Link to="/"><button type="button">Produtos</button></Link>
-          <Link to="/categorias"><button type="button">Categorias</button></Link>
-          <Link to="/sobre"><button type="button">Sobre</button></Link>         
-        </div>
-        <div className='flex space-x-2 p-8 text-white'>
+          <Link to="/" className='hover:text-blue-500 transition-colors'>
+            <motion.button 
+              type="button"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Produtos
+            </motion.button>
+          </Link>
+          <Link to="/categorias" className='hover:text-blue-500 transition-colors'>
+            <motion.button 
+              type="button"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Categorias
+            </motion.button>
+          </Link>
+          <Link to="/sobre" className='hover:text-blue-500 transition-colors'>
+            <motion.button 
+              type="button"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Sobre
+            </motion.button>
+          </Link>         
+        </motion.div>
+        <motion.div 
+          className='flex space-x-4 p-4 text-white'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5 }}
+        >
           <SignedOut>
-            <SignInButton />
-            <SignUpButton />
+            <motion.div 
+              className="flex space-x-2"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.7 }}
+            >
+              <SignInButton />
+              <SignUpButton />
+            </motion.div>
           </SignedOut>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
