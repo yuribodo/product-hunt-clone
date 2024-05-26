6283f8b55f6b40419b377d2e3b235817089179e2
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-05-25 21:44:24 -0400

    layout projects e get data do json

--- src/Components/ShowApss.tsx
+++ src/Components/ShowApss.tsx
@@ -1,55 +1,34 @@
-import image from '../../assets/Captura de tela 2024-05-24 174604.png'
+import React from 'react';
+import image from '../../assets/Captura de tela 2024-05-24 174604.png';
+import projects from '../../projects.json';
 
 const ShowApss = () => {
+  const projectsdata = projects;
+
   return (
     <div className="flex flex-col items-start p-10 mt-5">
-        <h1 className=" text-xl font-bold">O Próximo Grande App </h1>
-        
-            <div className="flex  ml-3 mt-6 items-start p-6">
-                <div>
-                    <img src={image} alt="logo" />
-                </div>
-                
-                <div>
-                    <h1 className=" text-lg font-semibold">Nome do app</h1>
-                    <p>descricao do app bla bla bla</p>
-                    <div className="flex space-x-2">
-                        <p>Ia</p>
-                        <p>API</p> 
-                    </div>
-                </div> 
-            </div>
-            <div className="flex  ml-3 mt-6 items-start p-6">
-                <div>
-                    <img src={image} alt="logo" />
-                </div>
-                
-                <div>
-                    <h1 className=" text-lg font-semibold">Nome do app</h1>
-                    <p>descricao do app bla bla bla</p>
-                    <div className="flex space-x-2">
-                        <p>Ia</p>
-                        <p>API</p> 
-                    </div>
-                </div> 
-            </div>
-            <div className="flex  ml-3 mt-6 items-start p-6">
-                <div>
-                    <img src={image} alt="logo" />
-                </div>
-                
-                <div>
-                    <h1 className=" text-lg font-semibold">Nome do app</h1>
-                    <p>descricao do app bla bla bla</p>
-                    <div className="flex space-x-2">
-                        <p>Ia</p>
-                        <p>API</p> 
-                    </div>
-                </div> 
-            </div>
-        
-    </div>
-  )
-}
+      <h1 className="text-xl font-bold">O Próximo Grande App</h1>
 
-export default ShowApss
+      {projectsdata.map((project) => (
+        <div key={project.id} className="flex w-full ml-3 mt-6 items-start p-6 border rounded-lg shadow-md">
+          <div>
+            <img src={image} alt="logo" className="w-16 h-16 object-cover" />
+          </div>
+          <div className="ml-4 flex-1">
+            <h1 className="text-lg font-semibold">{project.title}</h1>
+            <p>{project.description}</p>
+            <div className="flex space-x-2">
+              <p>Ia</p>
+              <p>API</p>
+            </div>
+          </div>
+          <div className="flex items-center border w-[40px] justify-center rounded border-black">
+            <p>{project.upvotes}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ShowApss;
