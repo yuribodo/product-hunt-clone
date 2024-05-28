06b5629bdf6a0b27a082de7086f6898ffaea281b
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-05-28 15:26:43 -0400

    reviwed product responsivenes

--- src/Components/ReviwedProducts.tsx
+++ src/Components/ReviwedProducts.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { FaStar, FaRocket, FaHeadphones, } from 'react-icons/fa';
+import { FaStar, FaRocket, FaHeadphones } from 'react-icons/fa';
 
 const ReviwedProducts = () => {
   const starCount = 5;
@@ -34,40 +34,40 @@ const ReviwedProducts = () => {
   ];
 
   return (
-    <div>
-      <h1 className="text-2xl text-center font-bold py-10">Produtos Revisados por nós</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl text-center font-bold py-4">Produtos Revisados por nós</h1>
 
       {products.slice(0, 3).map((product, index) => (
         <motion.div 
           key={index} 
-          className='flex justify-between items-center p-6 border-b border-gray-700'
+          className='flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 border-b border-gray-700 w-full'
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: index * 0.2 }}
         >
-          <div className="flex flex-col space-y-2">
+          <div className="flex flex-col space-y-2 flex-1">
             <h2 className='text-lg font-semibold'>{product.name}</h2>
             <p className='text-gray-400'>{product.description}</p>
             <div className='flex space-x-1'>{renderStars()}</div>
           </div>
-          <FaRocket className="text-4xl text-blue-500" />
+          <product.icon className="text-4xl text-blue-500" />
         </motion.div>
       ))}
 
-      <h1 className="text-center font-bold text-2xl py-10">Em breve</h1>
+      <h1 className="text-center font-bold text-2xl py-4">Em breve</h1>
 
       <motion.div 
-        className='flex justify-between items-center p-6 border-b border-gray-700'
+        className='flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 border-b border-gray-700 w-full'
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.6 }}
       >
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2 flex-1">
           <h2 className='text-lg font-semibold'>{products[3].name}</h2>
           <p className='text-gray-400'>{products[3].description}</p>
           <div className='flex space-x-1'>{renderStars()}</div>
         </div>
-        <FaHeadphones className="text-4xl text-blue-500" />
+          <FaHeadphones className="text-4xl text-blue-500" />
       </motion.div>
     </div>
   );
